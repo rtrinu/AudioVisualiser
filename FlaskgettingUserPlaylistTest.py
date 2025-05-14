18849eb3bf2efc0fb751492a0d5847459cb6488c
rtrinu <rbinu2004@gmail.com>
2025-05-14 17:04:52 +0100

    added new functions to get devices and currently playing devices

--- FlaskgettingUserPlaylistTest.py
+++ FlaskgettingUserPlaylistTest.py
@@ -21,7 +21,6 @@ AUTH_URL = "https://accounts.spotify.com/authorize"
 
 @app.route('/')
 def index():
-    # Create a login link page in html, css, js
     return render_template('index.html')
 
 
@@ -32,8 +31,8 @@ def login():
         'client_id': client_id,
         'response_type': 'code',
         'scope': scope,
-        'redirect_uri': REDIRECT_URI
-        #'show_dialog': 'true' # Optional: show the login dialog every time, delete later
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': 'true' # Optional: show the login dialog every time, delete later
     }
     auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
     return redirect(auth_url)
@@ -57,10 +56,22 @@ def callback():
         session['refresh_token' ]= token_info['refresh_token']
         session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
 
-        return redirect('/currentlyPlaying')
+        return redirect('/sdk')
+
+@app.route('/get_access_code')
+def get_access_code():
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect('/login')
+    expires_at = session.get('expires_at')
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh_token')
+    return access_token
 
 @app.route('/playlists')
 def get_playlists():
+    # Create a function that checks if the access token is expired and refreshes it if necessary so that
+    # i dont have to repeat the same code in multiple places
     access_token = session.get('access_token')
     if not access_token:
         return redirect('/login')
@@ -91,11 +102,9 @@ def currently_playing():
     
     response = requests.get(API_BASE_URL + 'me/player/currently-playing', headers=headers)
 
-    # Check for 204 (no content)
     if response.status_code == 204:
         return jsonify({'message': 'No track is currently playing.'})
 
-    # Check for other errors
     if response.status_code != 200:
         return jsonify({
             'error': 'Failed to fetch current track info',
@@ -113,6 +122,8 @@ def currently_playing():
         artist_name = current_track_info['item']['artists'][0]['name']
         track_url = current_track_info['item']['external_urls']['spotify']
         playing = current_track_info['is_playing']
+        session['track_name'] = track_name
+        session['artist_name'] = artist_name
         return jsonify({
             'track_name': track_name,
             'artist_name': artist_name,
@@ -140,6 +151,79 @@ def refresh_token():
         session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
         
         return redirect('/currentlyPlaying')
+    
+@app.route('/get_devices')
+def get_devices():
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session.get('expires_at'):
+        return redirect('/refresh_token')
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(API_BASE_URL + 'me/player/devices', headers=headers)
+    devices = response.json()
+    print(devices)
+    if 'devices' in devices:
+        device_names = [device['name'] for device in devices['devices']]
+        device_id = [device['id'] for device in devices['devices']]
+        device_active = [device['is_active'] for device in devices['devices']]
+        user_devices = []
+        for i in range(len(device_names)):
+            device_list = {
+                'device_name': device_names[i],
+                'device_id': device_id[i],
+                'device_active': device_active[i]
+            }
+            user_devices.append(device_list)
+        return jsonify(user_devices)
+    else:
+        return jsonify({'error': 'No devices found'}), 404
+    
+@app.route('/get_current_playing_device')
+def get_current_playing_device():
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session.get('expires_at'):
+        return redirect('/refresh_token')
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(API_BASE_URL + 'me/player', headers=headers)
+    if response.status_code == 204:
+        return jsonify({'message': 'No Device is currently playing.'})
+    current_device = response.json()
+    if 'device' in current_device:
+        device_name = current_device['device']['name']
+        device_id = current_device['device']['id']
+        session['device_id'] = device_id
+        session['device_name'] = device_name
+    else:
+        return jsonify({'error': 'No active device found'}), 404
+    
+
+@app.route('/sdk')
+def sdk():
+    currently_playing()
+    get_current_playing_device()
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect('/login')
+    expires_at = session.get('expires_at')
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh_token')
+    current_track_name = session.get('track_name')
+    current_artist_name = session.get('artist_name')
+    current_device_name = session.get('device_name')
+    current_device_id = session.get('device_id') 
+    return render_template('sdk.html', access_token=access_token,current_track_name=current_track_name,
+                           current_artist_name=current_artist_name,
+                           current_device_name=current_device_name,
+                           current_device_id=current_device_id)
 
 
     
